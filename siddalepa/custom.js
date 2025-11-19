58a6a0bbf3c614ea31f219a86f19de130cef886c
Rahal <121073543+rhl728@users.noreply.github.com>
2025-11-19 12:43:26 +0530

    added ID s for menu items and added js

--- siddalepa/custom.js
+++ siddalepa/custom.js
@@ -1,6 +1,6 @@
 const mainFn = () => {
   const header = document.querySelector("#home-page header#header");
-  header.classList.add("updatedHeader");
+  header?.classList.add("updatedHeader");
 
   window.addEventListener("scroll", function () {
     // console.log(">>>");
@@ -11,6 +11,16 @@ const mainFn = () => {
       header.classList.add("updatedHeader");
     }
   });
+
+  const currentUrl = window.location.href;
+  const url = new URL(currentUrl);
+  const currentPage = url.pathname;
+
+  if (currentPage == "/offers") {
+    const offerpage = document.querySelector("#header-offers");
+    // console.log(">>>", currentPage, offerpage);
+    offerpage.style.color = "rgb(115,40,22)";
+  }
 };
 
 document.addEventListener("oms_getTemplateListSuccess", function (e) {
